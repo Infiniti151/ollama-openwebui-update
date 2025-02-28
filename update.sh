cd6f79f14f4520f1ae0fa1a527584bc0c2f3d1ee
Infiniti151 <aadishchaubal@hotmail.com>
2025-02-28 16:49:12 -0500

    refactor

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ function update_ollama_models(){
 	ollama list | awk 'NR>1 {print $1}' | xargs ollama pull
 }
 
-function ollama_update(){
+function update_ollama(){
 	[ $(systemctl is-active ollama) = "active" ] && sudo systemctl stop ollama
 	curl -fsSL https://ollama.com/install.sh | sh &
 }
 
-function openwebui_update(){
+function update_openwebui(){
 	[ $(systemctl is-active docker) != "active" ] && sudo systemctl start docker
 	docker ps -a | awk '/open-webui/ {print $1}' | xargs docker stop | xargs docker remove -f
 	docker pull ghcr.io/open-webui/open-webui:latest &
@@ -24,23 +24,15 @@ function update(){
 	echo -e "------------------\e[32mUpdate Ollama Models\e[0m------------------"
 	update_ollama_models
 	echo -e "------------------\e[32mUpdate Ollama\e[0m-------------------------"
-	if [ $ollama_local != $ollama_latest ];then
-		ollama_update
-	else
-		echo "Ollama is the latest version"
-	fi
+	[ $ollama_local != $ollama_latest ] && update_ollama || echo "Ollama is the latest version"
 	echo -e "------------------\e[32mUpdate Open WebUI\e[0m---------------------"
-	if [ $local_image_digest != $remote_image_digest ];then
-		openwebui_update
-	else
-		echo "Open WebUI is the latest version"
-	fi
+	[ $local_image_digest != $remote_image_digest ] && update_openwebui || echo "Open WebUI is the latest version"
 }
 
 ollama_latest=$(git ls-remote -t https://github.com/ollama/ollama.git | awk -e '$2 !~ /rc|ci/ {sub(/refs\/tags\/v/,"");print $2}' | sort -V | awk 'END{print}')
 ollama_local=$(ollama --version | awk '{print $4}')
 
 remote_image_digest=$(regctl image digest ghcr.io/open-webui/open-webui:latest)
-local_image_digest=$(docker image inspect ghcr.io/open-webui/open-webui:latest --format '{{.RepoDigests}}' | awk -F@ '{sub(/]/,"");print $2}')
+local_image_digest=$(docker image inspect ghcr.io/open-webui/open-webui:latest -f '{{.RepoDigests}}' | awk -F@ '{sub(/]/,"");print $2}')
 
 update
